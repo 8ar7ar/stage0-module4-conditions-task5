09bc8ba8c29c9b4d23cf642ae51dba8447ee1def
8ar7ar <5ar7ar@mail.ru>
2023-10-22 23:39:18 +0500

    stage0-module4-conditions-task5 " 1st attempt" "locally tested"

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        boolean isLeapYear = (year%4 == 0 && year%100 !=0) || (year%400==0);
+        int leapYear = isLeapYear ? 1: 0;
+        switch (leapYear) {
+            case 1:
+                System.out.println("leap");
+                break;
+            default:
+                System.out.println("not leap");
+        }
+
     }
 }
